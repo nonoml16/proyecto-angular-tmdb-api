32f3614de26c72fa87c0ee3eda6cf4c7ee915096
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-11-08 14:30:02 +0100

    inicio historia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { ActorListComponent } from './components/actor-list/actor-list.component';
+import { PageHomeComponent } from './ui/page-home/page-home.component';
+import { PageNotFoundComponent } from './ui/page-not-found/page-not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    ActorListComponent,
+    PageHomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/actor-list.interface.ts
@@ -0,0 +1,2 @@
+export interface ActorList {
+}
