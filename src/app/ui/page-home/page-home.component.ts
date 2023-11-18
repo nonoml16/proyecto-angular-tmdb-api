fa308d55d58dfa6b51a9bc89d38c624dea4b8cc5
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-11-18 17:36:06 +0100

    funcionalidad agregada correctamente

--- src/app/ui/page-home/page-home.component.ts
+++ src/app/ui/page-home/page-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie-list.interface';
 import { MovieService } from 'src/app/service/movie.service';
 
 @Component({
@@ -8,13 +9,28 @@ import { MovieService } from 'src/app/service/movie.service';
 })
 export class PageHomeComponent implements OnInit {
   images: any[] = [];
+  moviePopularList: Movie[] = [];
+  movieTopRatedList: Movie[] = [];
+  movieUpcomingList: Movie[] = [];
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.movieService.getListP().subscribe(resp => {
+      this.moviePopularList = resp.results;
       this.images = resp.results;
-    })
+    });
+    this.movieService.getTopRatedListHome().subscribe(resp => {
+      this.movieTopRatedList = resp.results;
+      this.images = resp.results;
+    });
+    this.movieService.getUpcomingListHome().subscribe(resp => {
+      this.movieUpcomingList = resp.results;
+      this.images = resp.results;
+    });
   }
   
+  getImageMovie(profile_path: string | null): string {
+    return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2${profile_path}`;
+  }
 }
